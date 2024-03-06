e6f3c67d0630ae400a4b5edb0d7cc1d19788f30f
Nurupo <191977649@qq.com>
2024-03-06 19:36:03 +1100

    add

---
+++ model/harmonic.py
@@ -0,0 +1,9 @@
+from pychord import Chord
+from feature.harmonic import note_frequency,find_harmonic_tension
+class Harmonic:
+    def __init__(self,chord_name):
+        chord_name = chord_name.replace(":","")
+        self.chord = Chord(chord_name)
+        self.chord_notes = self.chord.components()
+        self.chord_frequencies = [note_frequency(note,4) for note in self.chord_notes]
+        self.tension = find_harmonic_tension(self.chord_frequencies)
--- task/dtw_optimization/optmization.py
+++ task/dtw_optimization/optmization.py
@@ -1,6 +1,10 @@
 from feature import extract as feature
+from model.harmonic import *
 from tslearn.metrics import dtw_path,dtw
 from model.song import *
+from pychord import Chord
+import matplotlib.pyplot as plt
+
 def summaryChordPattern(chordsArray,WINDOW = 16):
     if not chordsArray: return []
     print("Extracting Summary Chord Pattern ...")
@@ -93,10 +97,97 @@ def summaryChordPattern(chordsArray,WINDOW = 16):
             })
            # print(pattern,len(details['matches']))
     #pattern_freq = {pattern: len(details['matches']) for pattern, details in matched_patterns.items() if len(details['matches']) > 0}
+    #plot
+    # Plotting the signal
+    x_values = range(len(chord_theta_ls))
+    filtered_patterns = {pattern: details for pattern, details in
+                         sorted(matched_patterns.items(), key=lambda item: len(item[1]['matches']), reverse=True) if
+                         len(details['matches']) > 0}
+
+    total_pattern_groups = len(filtered_patterns)
+
+    colormap = plt.cm.get_cmap('tab10', total_pattern_groups)  # 'tab10' has a wide range of distinct colors
+
+    color_idx = 0
+
+    fig, axs = plt.subplots(total_pattern_groups, 1, figsize=(15, 2 * total_pattern_groups), squeeze=True)
+    axs = axs.flatten()
+
+    # Plot each unique pattern and its matches in a separate subplot
+    for (pattern, details), ax in zip(filtered_patterns.items(), axs):
+        # Get color from the colormap, use modulo to cycle through colors if there are more patterns than colors
+        color = colormap(color_idx % colormap.N)
+        color_idx += 1  # Increment color index for the next distinct pattern
+
+        # Plot the entire signal in a neutral color in the background
+        neutral_color = 'grey'
+        ax.plot(x_values, chord_theta_ls, marker='o', alpha=0.5, color=neutral_color, drawstyle='steps-post',
+                markersize=2, zorder=1)
+
+        # Reference pattern segment
+        ref_start_idx = details['start_ref']
+        ref_end_idx = details['end_ref']
+        ax.plot(x_values[ref_start_idx:ref_end_idx + 1],chord_theta_ls[ref_start_idx:ref_end_idx + 1], marker='o',
+                color=color, drawstyle='steps-post', markersize=2, zorder=2, label='Reference')
+
+        # Plot each matching segment
+        for match in details['matches']:
+            search_start_idx = match['start_search']
+            search_end_idx = match['end_search']
+            ax.plot(x_values[search_start_idx:search_end_idx + 1],
+                    chord_theta_ls[search_start_idx:search_end_idx + 1],
+                    marker='o', linewidth=3, color=color, drawstyle='steps-post', markersize=2, zorder=2, label='Match')
+
+        # Set title and labels for each subplot
+        ax.set_title(f"Pattern: {pattern} - {len(details['matches'])} Matches")
+        ax.set_ylabel('Angle')
+        ax.set_xticks(x_values)
+        ax.set_xticklabels(chord_beat_ls, rotation='vertical', fontsize=8)
+        ax.set_xlim(left=0, right=max(x_values))
+        ax.set_ylim(min(chord_theta_ls), max(chord_theta_ls))
+        # ax.legend()
+
+    # Finalizing the plot
+    plt.tight_layout()
+    plt.show()
+
 
     return output
 
+
+def detect_ending_cadence(chord_progression):
+    ending_cadences_major = {
+        ["V","I"]: 4,   # "Authentic (Perfect) Cadence"
+        ["IV","I"]:3,   #"Plagal Cadence"
+        ["*","V"]: 1,   #"Half Cadence"
+        ["V","vi"]:2,   #"Deceptive (Interrupted) Cadence"
+    }
+
+    ending_cadences_minor = {
+        ["V", "i"]: 4,  #"Authentic (Perfect) Cadence",
+        ["iv", "i"]:3,  #"Plagal Cadence",
+        ["*", "V"]: 1,  #"Half Cadence",
+        ["V", "VI"]:2,  #"Deceptive (Interrupted) Cadence",
+    }
+
+
 if __name__ == '__main__':
     song = Song.from_h5('/Users/nurupo/Desktop/dev/music4all/europe/The Final Countdown [NNiTxUEnmKI].h5')
-    result = summaryChordPattern(song.chord)
-    
+
+    result = summaryChordPattern(song.chord_transposed,WINDOW=16)
+    print("_________")
+    overall = 0
+    for pat in result:
+        progression = pat["pattern"]
+        roman_label = pat["roman_label"]
+        tension = 0
+        for chord in progression:
+            harmonic = Harmonic(chord)
+            #print(harmonic.tension*1000)
+            tension += harmonic.tension*1000
+        overall = overall + tension
+        print(tension)
+    print(f"overall:{overall}")
+    #print(result)
+    #c = Chord("Cmaj")
+    #print(c.components())
